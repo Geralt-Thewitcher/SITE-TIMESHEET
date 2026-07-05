cdb0a5273618b307a4436ee8e137631e4dd41181
Geralt-Thewitcher <subasc04@gmail.com>
2026-07-05 14:02:57 +0530

    Add MainActivity class with onCreate method

--- app/src/main/java/com/shivkamal/sitetimesheet/MainActivity.kt
+++ app/src/main/java/com/shivkamal/sitetimesheet/MainActivity.kt
@@ -1,1 +1,12 @@
+package com.shivkamal.sitetimesheet
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
